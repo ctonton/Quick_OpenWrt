a98f6eef66a591faa971b794301a872f8df5c0b9
ctonton <51173996+ctonton@users.noreply.github.com>
2024-12-11 15:00:08 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,56 +1,37 @@
 #!/bin/bash
 
 ## host files after build or comment out to disable
-hst='yes'
+#hst='yes'
 
 ## address of imagebuilder package or comment out to chose later
-url='https://downloads.openwrt.org/releases/23.05.5/targets/ramips/mt7621/openwrt-imagebuilder-23.05.5-ramips-mt7621.Linux-x86_64.tar.xz'
+#url='https://downloads.openwrt.org/releases/23.05.5/targets/ramips/mt7621/openwrt-imagebuilder-23.05.5-ramips-mt7621.Linux-x86_64.tar.xz'
 
 ## model of router to build for or comment out to chose later
-mod='zbtlink_zbt-we1326'
+#mod='zbtlink_zbt-we1326'
 
 ## set a default password and dropbear key or comment out to leave blank
 pas='password'
-key='ssh-rsa..........................'
+#key='ssh-rsa..........................'
 
 ## custom repository to pull packages from or comment out to disable
-rep='src/gz IceG_repo https://github.com/4IceG/Modem-extras/raw/main/myrepo'
+#rep='src/gz IceG_repo https://github.com/4IceG/Modem-extras/raw/main/myrepo'
+
 ## place any additional packages in the same directory as this build script
 
 ## packages to install
 opk='\
-block-mount \
-kmod-fs-exfat \
-kmod-fs-ext4 \
-kmod-fs-vfat \
-kmod-fs-xfs \
-kmod-usb2 \
-kmod-usb3 \
-kmod-usb-net-qmi-wwan \
-kmod-usb-serial-option \
-kmod-usb-serial-qualcomm \
-kmod-usb-storage \
 luci \
-luci-app-3ginfo-lite \
-luci-app-ksmbd \
-luci-app-modemband \
 luci-app-opkg \
-luci-app-sms-tool-js \
-luci-app-watchcat \
-luci-proto-qmi \
-luci-proto-wireguard \
-nano \
-ntfs-3g \
-socat'
+nano'
 
 ## set TTL for celular data or comment out to diable
-ttl='65'
+#ttl='65'
 
 ## watchcat command to reboot the cellular modem or comment out to diable
-wac='echo -e "AT+CFUN=1,1" >/dev/ttyUSB2'
+#wac='echo -e "AT+CFUN=1,1" >/dev/ttyUSB2'
 
 ## schedule tasks or comment out the next line to disable
-crn='yes'
+#crn='yes'
 tasks() { cat <<EOT
 59 7 * * 1 sleep 70 && touch /etc/banner && reboot
 EOT
@@ -59,68 +40,10 @@ EOT
 ## set uci defaults
 deflist() { cat <<EOT
 uci set system.@system[0].hostname=''
-
-uci set network.lan.ipaddr='10.1.1.1'
-uci add_list network.lan.dns='208.67.222.123'
-uci add_list network.lan.dns='208.67.220.123'
-uci set network.lan.delegate='0'
-uci delete network.lan.ip6assign
-uci set network.mobile='interface'
-uci set network.mobile.proto='qmi'
-uci set network.mobile.device='/dev/cdc-wdm0'
-uci set network.mobile.apn=''
-uci set network.mobile.auth='none'
-uci set network.mobile.pdptype='ipv4'
-uci set network.mobile.peerdns='0'
-uci add_list network.mobile.dns='208.67.222.123'
-uci add_list network.mobile.dns='208.67.220.123'
-uci set network.wg0=interface
-uci set network.wg0.proto='wireguard'
-uci set network.wg0.private_key=''
-uci set network.wg0.mtu='1340'
-uci add_list network.wg0.addresses=''
-uci add_list network.wg0.addresses=''
-uci add network wireguard_wg0
-uci set network.@wireguard_wg0[0].description=''
-uci set network.@wireguard_wg0[0].public_key=''
-uci set network.@wireguard_wg0[0].private_key=''
-uci set network.@wireguard_wg0[0].preshared_key=''
-uci set network.@wireguard_wg0[0].endpoint_host=''
-uci set network.@wireguard_wg0[0].endpoint_port='51820'
-uci set network.@wireguard_wg0[0].persistent_keepalive='25'
-uci add_list network.@wireguard_wg0[0].allowed_ips=''
-
-uci add_list firewall.@zone[0].network='wg0'
-uci add_list firewall.@zone[1].network='mobile'
-
-uci add dhcp host
-uci set dhcp.@host[0].name=''
-uci set dhcp.@host[0].ip=''
-uci set dhcp.@host[0].mac=''
-uci add dhcp host
-uci set dhcp.@host[1].name=''
-uci set dhcp.@host[1].ip=''
-uci set dhcp.@host[1].mac=''
-
 uci set wireless.radio0.disabled='0'
 uci set wireless.radio0.country='US'
-uci set wireless.default_radio0.ssid=''
-uci set wireless.default_radio0.encryption='psk2'
-uci set wireless.default_radio0.key=''
 uci set wireless.radio1.disabled='0'
 uci set wireless.radio1.country='US'
-uci set wireless.default_radio1.ssid=''
-uci set wireless.default_radio1.encryption='psk2'
-uci set wireless.default_radio1.key=''
-
-uci set watchcat.@watchcat[0].period='5m'
-uci set watchcat.@watchcat[0].mode='run_script'
-uci set watchcat.@watchcat[0].pinghosts='1.1.1.1'
-uci set watchcat.@watchcat[0].script='/usr/share/watchcat/restart.sh'
-uci set watchcat.@watchcat[0].addressfamily='ipv4'
-uci set watchcat.@watchcat[0].pingperiod='1m'
-uci set watchcat.@watchcat[0].pingsize='standard'
-uci set watchcat.@watchcat[0].interface='wwan0'
 EOT
 }
 
