b19e8e8ab8afa3f680aa9cb64ceb4cece7040c71
ctonton <51173996+ctonton@users.noreply.github.com>
2026-01-10 13:38:52 -0500

    Refactor build script for improved error handling

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ MODL='tplink_tl-wr902ac-v3'
 depA=(curl zstd openssl build-essential file libncurses-dev zlib1g-dev gawk git gettext libssl-dev xsltproc rsync wget unzip python3 python3-setuptools)
 for dep in ${depA[@]} ; do dpkg -l "$dep" 2>/dev/null | grep -q '^ii' || i=1; done
 if [ "$i" = 1 ] ; then
-  sudo apt update && sudo apt -y install "${depA[@]}" || exit $?
+  sudo apt update && sudo apt -y install ${depA[@]} || exit $?
 fi
 
 # set root password
@@ -29,47 +29,54 @@ until h=$(openssl passwd -5) ; do echo ; done
 p="root:$h:$(($(date +%s) / 86400)):0:99999:7:::"
 
 # download and extract imagebuilder package
-versA=($(curl -s "https://downloads.openwrt.org/releases/" | pup 'tr td a text{}' | grep '^[0-9]' && echo "snapshots"))
-if printf "%s\\n" ${versA[@]} | grep -q "$VERS" ; then
+versA=($(curl -s "https://downloads.openwrt.org/releases/" | pup 'tr td a text{}' | grep '^[0-9]'))
+[ -z "${versA[*]}" ] && echo -e "Host \"https://downloads.openwrt.org/releases/\" is unreachable." && exit 127
+versA+=("snapshots")
+if [[ " ${versA[*]} " =~ " ${VERS} " ]] ; then
   v="$VERS"
 else
   echo
   PS3="Select openwrt version: "
   select v in ${versA[@]} ; do break ; done
-  sed -i "s/^VERS=.*/VERS=\'$v\'/" $0
+  sed -i "s/^VERS=.*/VERS='$v'/" $0
 fi
 [ "$v" = "snapshots" ] && r="$v" || r="releases/$v"
 archA=($(curl -s "https://downloads.openwrt.org/$r/targets/" | pup 'tr a text{}'))
-if printf "%s\\n" ${archA[@]} | grep -q "$ARCH" ; then
+[ -z "${archA[*]}" ] && echo -e "Host \"https://downloads.openwrt.org/$r/targets/\" is unreachable." && exit 127
+if [[ " ${archA[*]} " =~ " ${ARCH} " ]] ; then
   a="$ARCH"
 else
   echo
   PS3="Select device architecture: "
   select a in ${archA[@]} ; do break; done
-  sed -i "s/^ARCH=.*/ARCH=\'$a\'/" $0
+  sed -i "s/^ARCH=.*/ARCH='$a'/" $0
 fi
 chipA=($(curl -s "https://downloads.openwrt.org/$r/targets/$a/" | pup 'tr a text{}'))
-if printf "%s\\n" ${chipA[@]} | grep -q "$CHIP" ; then
+[ -z "${chipA[*]}" ] && echo -e "Host \"https://downloads.openwrt.org/$r/targets/$a/\" is unreachable." && exit 127
+if [[ " ${chipA[*]} " =~ " ${CHIP} " ]] ; then
   c="$CHIP"
 else
   echo
   PS3="Select processor model: "
   select c in ${chipA[@]} ; do break; done
-  sed -i "s/^CHIP=.*/CHIP=\'$c\'/" $0
+  sed -i "s/^CHIP=.*/CHIP='$c'/" $0
 fi
 f=$(curl -s "https://downloads.openwrt.org/$r/targets/$a/$c/" | pup 'tr a text{}' | grep 'imagebuilder')
+[ -z "$f" ] && echo -e "Host \"https://downloads.openwrt.org/$r/targets/$a/$c/\" is unreachable." && exit 127
 [ -f "$f" ] || wget "https://downloads.openwrt.org/$r/targets/$a/$c/$f" || exit $?
 d="${f%.tar.*}"
 [ -d "$d" ] || tar -axf "$f"
 
 # setup extra repositories
 if [ -n "$rep" ] ; then
-  sed -i 's/^option/#option/' "$d"/repositories.conf
+  sed -i 's/^option/#option/' "$d/repositories.conf"
   grep -q "$rep" "$d/repositories.conf" || echo "$rep" >>"$d/repositories.conf"
 fi
 
 # copy packages and files
 mkdir -p bin
+[ -f "uci" ] || touch uci
+[ -f "opkg" ] || touch opkg
 rm -rf "$d/bin" "$d/files" "$d/packages"
 [ -d "packages" ] && cp -r packages "$d"/ || mkdir "$d/packages"
 [ -d "files" ] && cp -r files "$d"/ || mkdir "$d/files"
@@ -80,18 +87,19 @@ if ! echo $p | grep -q ':<NULL>:' ; then
   echo 'exit 0' >>"$d/files/etc/uci-defaults/10-pas"
   chmod +x "$d/files/etc/uci-defaults/10-pas"
 fi
-if [ -f "uci" ] ; then
+if [ -s "uci" ] ; then
   echo '#!/bin/sh' >"$d/files/etc/uci-defaults/20-uci"
-  cat uci >>"$d/files/etc/uci-defaults/20-uci"
+  cat ./uci >>"$d/files/etc/uci-defaults/20-uci"
   echo -e "uci commit\nexit 0" >>"$d/files/etc/uci-defaults/20-uci"
   chmod +x "$d/files/etc/uci-defaults/20-uci"
 fi
+[ -s "opkg" ] && opkgA=($(cat opkg | xargs))
 
 # build images
 cd "$d"
 [ -f ".profiles.mk" ] || make info &>/dev/null
 modlA=($(grep 'PROFILE_NAMES = ' .profiles.mk | sed 's/PROFILE_NAMES = //;s/DEVICE_//g'))
-if printf "%s\\n" ${modlA[@]} | grep "$MODL" ; then
+if [[ " ${modlA[*]} " =~ " ${MODL} " ]] ; then
   m="$MODL"
 else
   echo
@@ -101,9 +109,9 @@ else
   PS3="Select router model: "
   select r in $(printf "%s\\n" ${modlA[@]} | cut -d '_' -f 2 | sort) ; do break; done
   m="$b"_"$r"
-  sed -i "s/^MODL=.*/MODL=\'$m\'/" $0
+  sed -i "s/^MODL=.*/MODL='$m'/" ../$0
 fi
-make image PROFILE="$m" PACKAGES="$(cat ../opkg 2>/dev/null | xargs)" FILES="files" || exit $?
+make image PROFILE="$m" PACKAGES="${opkgA[*]}" FILES="files" || exit $?
 
 # copy build
 cp -f "bin/targets/$a/$c"/* ../bin/
